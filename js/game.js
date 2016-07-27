489da3be2bb93d8803fb5fa1d4e355f798b12155
aldompe95 <aldompe95@gmail.com>
2016-07-27 16:00:22 -0500

    finish validations

--- js/game.js
+++ js/game.js
@@ -1,13 +1,18 @@
+"use strict";
+
 //Just numbers
 function isNumber(evt){
   evt = evt || window.event;
   var charCode = evt.which || evt.keyCode; 
   return charCode <= 31 || (charCode >= 48 && charCode <= 57);
 }
-//Paste disable
+//Paste and drop disable
 window.onload = function(){ 
   var rows = document.querySelector('.rows');
   var columns = document.querySelector('.columns');
-  rows.onpaste = function(e){e.preventDefault();}
-  columns.onpaste = function(e){e.preventDefault();} 
+  columns.ondrop = rows.ondrop = columns.onpaste = rows.onpaste = function disableEnvent(){return false;} 
+}
+//Max number
+function maxNumber(number){
+  return (number <= 30);
 }
