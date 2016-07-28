73fa0548cf82f7d83a4d6352ae49b819fa2f5ed9
aldompe95 <aldompe95@gmail.com>
2016-07-28 17:45:45 -0500

    finished

--- js/game.js
+++ js/game.js
@@ -19,7 +19,7 @@ function maxNumber(number){return (number <= 30);}
 function createWorld(){
   var rows = document.querySelector('.rows').value;
   var columns = document.querySelector('.columns').value;
-  if (maxNumber(rows) == false || maxNumber(columns) == false){
+  if (maxNumber(rows) == false || maxNumber(columns) == false){ //I dont know how to do this without if
     alert("Max 30");
     return false;
   }
@@ -50,25 +50,65 @@ function printMatrix(matrix){
     for (var j = 0; j < matrix[0].length; j++){
       html += ({
         0: "<td class='cell' onclick='activeCell("+i+","+j+","+active+")'>" +"</td>",
-        1: "<td class='cell active' onclick='activeCell("+i+","+j+","+active+")'>" +"</td>",
+        1: "<td class='cell active' onclick='activeCell("+i+","+j+","+active+")'>" +"</td>"
       })[matrix[i][j]];
     }
     html += "</tr>";
   }
   html += "</table>";
-  html += " <button onclick='' type='button'>PLAY</button>";
+  html += " <button onclick='startGame()' type='button'>PLAY</button>";
   document.querySelector(".world").innerHTML = html;
 }
 //Active cells
 function activeCell(x,y,active){
-  matrix[x][y] = ({
-    0: 1,
-    1: 0,
-  })[matrix[x][y]];
-  printMatrix(matrix);
+  if(active == true){ //I dont know how to do this without if(is the same problem)
+    matrix[x][y] = ({0: 1, 1: 0})[matrix[x][y]];
+    printMatrix(matrix);
+  }
 }
 //Start game
 function startGame(){
   active = false; // You cant active cells anymore :(
+  for (var i = 0; i < matrix.length; i++){
+    for (var j = 0; j < matrix[0].length; j++){
+      var neighbours = checkNeighbours(i,j);
+      (neighbours < 2) && (function(){mirror[i][j] = 0})();
+      (neighbours > 3) && (function(){mirror[i][j] = 0})();
+      (neighbours == 3) && (function(){mirror[i][j] = 1})();
+    }
+  }
+  mirroring(mirror);
+  printMatrix(matrix);
+  setTimeout(function(){startGame(matrix)},1000); // 1000ms = 1s
 }
-
+//Check neighbours
+function checkNeighbours(i,j){
+  var n = 0;
+  //LEFT
+  (j - 1 >= 0) && ((matrix[i][j-1]) && (function(){n += 1;})());
+  //RIGHT
+  (j + 1 < matrix[0].length) && ((matrix[i][j+1]) && (function(){n += 1;})());
+  //TOP
+  (i - 1 >= 0) && ((matrix[i-1][j] == 1) && (function(){n += 1;})());
+  //BOTTOM
+  (i + 1 < matrix.length) && ((matrix[i+1][j] == 1) && (function(){n += 1;})());
+  //TOP LEFT
+  var a = i-1;
+  (i - 1 >= 0) && ((j - 1 >= 0) && ((matrix[a][j-1] == 1) && (function(){n += 1;})() ));
+  //TOP RIGHT
+  (i - 1 >= 0) && ((j + 1 < matrix[0].length) && ((matrix[a][j+1] == 1) && (function(){n += 1;})()));
+  //BOTTOM-LEFT
+  var b = i+1;
+  (i + 1 < matrix.length) && ((j - 1 >= 0) && ((matrix[b][j-1] == 1) && (function(){n += 1;})()));
+  //BOTTOM-RIGHT
+  (i + 1 < matrix.length) && ((j + 1 < matrix[0].length) && ((matrix[b][j+1] == 1) && (function(){n += 1;})()));
+  return n;
+}
+//mirroring matrixs
+function mirroring(mirror){
+  for (var i = 0; i < matrix.length; i++){
+    for (var j = 0; j < matrix[0].length; j++){
+      matrix[i][j] = mirror[i][j];
+    }
+  }
+}
